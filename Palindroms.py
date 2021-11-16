705ce00d1e1066d1ee1ae84584e33893955dbec3
triman <tri.thanh.do@gmail.com>
2021-11-16 11:35:08 -0800

    commit Palindroms

--- Palindroms.py
+++ Palindroms.py
@@ -1,2 +1,20 @@
-w = input("Enter a word ")
-print (w, " ")
+word = input("Enter a word ")
+ori = []
+for w in word:
+    ori.append(w)
+oriTmp = ori[::-1]
+l = len(ori)
+l = l - 1
+pal = False
+while  -1 < l:
+    oT = oriTmp.pop()
+    if(ori[l] != oT):
+        pal = False
+        break
+    else:
+        pal = True
+    l -= 1
+if(pal):
+    print(f'{word} is a palindromes')
+else:
+    print(f'{word} is not a palindromes')
