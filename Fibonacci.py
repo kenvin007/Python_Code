2f5c6c08075da2fbf71861c7e1aeda31c6ed035b
triman <tri.thanh.do@gmail.com>
2021-11-16 15:12:24 -0800

    small change

--- Fibonacci.py
+++ Fibonacci.py
@@ -3,9 +3,9 @@ if(Seq == 1):
     Fib = 1
     print(Fib)
 else:
+    print ('1',end=' ')
     li = []
 # 0,1,2,3,5,8,13
-    curr = 1
     li.append(1)
     li.append(1)
     for i in range(0,Seq):
