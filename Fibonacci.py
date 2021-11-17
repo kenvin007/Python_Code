9f8f52b3d36c79cbcbba92f38bcc37194e50ca85
triman <tri.thanh.do@gmail.com>
2021-11-17 10:43:42 -0800

    Add function

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,15 +1,17 @@
-Seq = int(input("enter sequence: "))
-if(Seq == 1):
-    Fib = 1
-    print(Fib)
-else:
-    print ('1',end=' ')
-    li = []
-# 0,1,2,3,5,8,13
-    li.append(1)
-    li.append(1)
-    for i in range(0,Seq):
-        val = li[i] + li[i+1]
-        li.append(val)
-        print(val, end=" ")
-print()
+def fib(Seq):
+    #Seq = int(input("enter sequence: "))
+    if(Seq == 1):
+        Fib = 1
+        print(Fib)
+    else:
+        print ('1',end=' ')
+        li = []
+        # 0,1,2,3,5,8,13
+        li.append(1)
+        li.append(1)
+        for i in range(0,Seq):
+            val = li[i] + li[i+1]
+            li.append(val)
+            print(val, end=" ")
+        print()
+fib(9)
