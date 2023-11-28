1ae48c61d0fe4e8493e37001eebe839bd4efeba4
Tri Do <trido@Tris-MacBook-Pro-2.local>
2023-11-28 00:33:03 -0800

    test perl

--- test.pl
+++ test.pl
@@ -1,2 +1,27 @@
 #!/usr/bin/perl
-print "Hello perl\n";
+open(HDL,"curl https://www.wsj.com/tech/ai/microsoft-needs-a-better-seat-at-openais-table-64bc3c3b?mod=tech_lead_story | pandoc -f html -t plain |");
+   while($tmp=<HDL>){
+      chop $tmp;
+      next if $tmp =~ /s+/;
+      next if $tmp eq "";
+      @arrTmp = split('\s+',$tmp);
+      foreach $a (@arrTmp){
+         if($uniqWord{$a}){
+            $uniqWord{$a} += 1;
+         }else{
+            $uniqWord{$a} = 1;
+         }
+      }
+  }
+close HDL;
+#while(($k,$v) = each %uniqWord){
+#   print "$k = $v\n"
+#}
+foreach my $k (sort { $uniqWord{$a} <=> $uniqWord{$b}} keys %uniqWord){
+   print "$k";
+   for($i=0;$i<$uniqWord{$k};$i++){
+      print "*";
+   }
+   print "\n";
+}
+
